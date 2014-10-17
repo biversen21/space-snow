3d5bc9a63a2e5a9763fa249e143660d493e9d9b2
Brian Iversen <biversen21@gmail.com>
2014-10-16 17:22:53 -0700

    fixes timed adds and starts playerScore model and view

--- client/models/Building.js
+++ client/models/Building.js
@@ -2,5 +2,12 @@ var Building = Backbone.Model.extend({
 
   addToBoard: function(){
   	this.trigger('addToBoard', this);
-  }
+  },
+	
+	startResources: function(){
+		var that = this;
+		setInterval(function(){
+      that.trigger('increaseResources', that);
+		}, that.attributes.time);
+	}
 });

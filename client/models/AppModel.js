8dfdfbed579089e22921117ae202fd392a009e23
Brian Iversen <biversen21@gmail.com>
2014-10-20 14:00:15 -0700

    adds resource check when trying to buy new building

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,14 +1,31 @@
 var AppModel = Backbone.Model.extend({
   initialize: function(params) {
     this.set('playerBuildings', new PlayerBuildings());
-    // this.set('playerScore', new PlayerScore());
+    this.set('playerScore', new PlayerScore());
+    
+    var playerScore = this.get('playerScore');
+    playerScore.fetch({
+      success: function(player) {
+        var playerScoreView = new PlayerScoreView({model: player});
+        // setInterval(function(){
+        //   playerScore.fetch();
+        //   console.log('refetch');
+        // }, 1000);
+      }
+    });
 		
     params.buildingLibrary.on('addToBoard', function(building) {
+      var currentScore = this.get('playerScore');
       var buildingCollection = this.get('playerBuildings');
-      if(buildingCollection.length < 6) {
+      
+      var affordBuilding = currentScore.checkResource(building);
+      
+      if ((buildingCollection.length < 6) && (affordBuilding)) {
         buildingCollection.add(building.toJSON());
+      } else if (!affordBuilding){
+        alert('You can\'t afford that!')
       } else {
-        alert('Too many buildings asshole!');
+        alert('Too many buildings asshole!');        
       }
     }, this);
   }

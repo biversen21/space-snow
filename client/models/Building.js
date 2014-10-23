19ceed5cb51dae613d7736a95e06feab42d621a9
Tess Myers <tess@tessmyers.com>
2014-10-23 16:14:55 -0700

    pares down css

--- client/models/Building.js
+++ client/models/Building.js
@@ -1,8 +1,15 @@
 var Building = Backbone.Model.extend({
-  
+
+  // current: undefined,
+
   url: '/player',
 
   addToBoard: function(){
   	this.trigger('addToBoard', this);
+  },
+
+  drag: function(clickEvent, that){
+    that.clone(true).addClass('ghost').appendTo('body');
   }
+
 });

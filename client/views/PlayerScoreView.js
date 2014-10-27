5ca9c465c5eb6cd2ff2f43e9f7f112d44c560773
Shan Swami <shan.swami@gmail.com>
2014-10-27 15:23:47 -0700

    bar chart added to player score view

--- client/views/PlayerScoreView.js
+++ client/views/PlayerScoreView.js
@@ -20,14 +20,25 @@ var PlayerScoreView = Backbone.View.extend({
     // builds html based on player attributes
     var playerStats = "<h3>Statistics</h3>";
     playerStats += '<span class="playerStats">Name: ' + this.model.attributes.name + '<span><br>';
+    var data = [];
     for (var resource in this.model.attributes.resources) {
-      playerStats += '<span>' + resource + ': ' + this.model.attributes[resource] + '<span><br>';
+      // playerStats += '<span>' + resource + ': ' + this.model.attributes[resource] + '<span><br>';
+      data.push(this.model.attributes[resource])
     }
 
     // checks if player has resources and refinery to add refine option
     if (this.model.attributes.resources.minerals > 50 && this.model.checkRefinery()) {
       playerStats += '<button class="refine">Refine</button>';
     }
+
+    // var body = d3.select("body");
+    // var div = body.append("div");
+    // d3.select(".chart")
+    // .selectAll("div")
+    // .data(data)
+    // .enter().append("div")
+    // .style("width", function(d) { return d * 10 + "px"; })
+    // .text(function(d) { return d; });
 
     return this.$el.append(playerStats);
   }

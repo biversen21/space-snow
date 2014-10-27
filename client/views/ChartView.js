a56078facad11fe48f9ae258a259a7712ab5ebfd
Shan Swami <shan.swami@gmail.com>
2014-10-27 10:36:45 -0700

    chartview good to go

--- client/views/ChartView.js
+++ client/views/ChartView.js
@@ -122,22 +122,41 @@ $(function() {
   });
   var DonutChartView = ChartView.extend({
     draw: function() {
+      var color = d3.scale.category10();
+      var pie = d3.layout.pie().sort(null);
+
+      var arc = d3.svg.arc()
+        .innerRadius(radius - 100)
+        .outerRadius(radius - 75);
       
+      var svg = d3.select("body")
+        .append("svg")
+        .attr("class", "donut-chart")
+        .attr("width", this.width)
+        .attr("height", this.height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+        var path = svg.selectAll("path")
+          .data(pie(this.data))
+          .enter().append("path")
+          .attr("fill", function(d, i) { return color(i); })
+          .attr("d", arc);
     }
   })
-  var chart_one_data = [3, 8, 12, 7, 17];
-  var chart_two_data = [4, 10, 13, 14, 7];
+  // var chart_one_data = [3, 8, 12, 7, 17];
+  // var chart_two_data = [4, 10, 13, 14, 7];
     
-  var chart_one = new BarChartView({
-    el: '#one',
-    data: chart_one_data,
-    base_height: 220
-  }).render();
+  // var chart_one = new BarChartView({
+  //   el: '#one',
+  //   data: chart_one_data,
+  //   base_height: 220
+  // }).render();
 
-  var chart_two = new BarChartView({
-    el: '#two',
-    data: chart_two_data,
-    base_height: 220
-  }).render();
+  // var chart_two = new BarChartView({
+  //   el: '#two',
+  //   data: chart_two_data,
+  //   base_height: 220
+  // }).render();
     
 });

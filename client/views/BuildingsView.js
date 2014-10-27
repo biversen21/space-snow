28e4b9424a67d6dc34244682f8338ea61f44f7f7
Tess Myers <tess@tessmyers.com>
2014-10-27 12:07:39 -0700

    (feat) adds building descriptions

--- client/views/BuildingsView.js
+++ client/views/BuildingsView.js
@@ -1,22 +1,21 @@
 var BuildingsView = Backbone.View.extend({
-  
+
   el: '.build',
-  
+
   tagName: 'div',
-	
-	initialize: function(){
-		this.render();
-	},
-	
-	render: function(){
-		this.$el.children().detach();
-		this.$el.empty();
-    
+
+  initialize: function(){
+    this.render();
+  },
+
+  render: function(){
+    this.$el.children().detach();
+    this.$el.empty();
     this.$el.append(
-		  this.collection.map(function(building){
-		  	return new BuildingEntryView({model: building}).render();
-		  })
-		);
-		
-	}
+      this.collection.map(function(building){
+        return new BuildingEntryView({model: building}).render();
+      })
+    );
+
+  }
 });

6858c6db2c5edc732ee293a205f6c381d38933c3
Brian Iversen <biversen21@gmail.com>
2014-10-27 17:22:26 -0700

    adds final docs and text to bar charts

--- client/collections/PlayerBuildings.js
+++ client/collections/PlayerBuildings.js
@@ -2,6 +2,7 @@ var PlayerBuildings = Buildings.extend({
 	
   url: '/game',
 	
+  // returns total size of all player buildings to determine whether new building can be added
   checkSize: function(){
     var totalSize = 0;
     for (var i = 0; i < this.models.length; i++) {
